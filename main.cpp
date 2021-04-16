1fd2e7ace0101a54f0bdd418d1cc7d1c6cc67572
Jorge Hoste <>
2021-04-16 21:19:03 +0000

    seguemcia de if´s para verificar a letra e jogar na tela a tecla que deve ser apertada

--- main.cpp
+++ main.cpp
@@ -15,4 +15,87 @@ int main() {
     P.erase(49);
   }
   cout << "A palavra digitada é : " << P << endl;
+
+  for (int i =0; i < P.length(); i++){
+
+    if(P[i] == 'a'){
+      cout << "#2=1"<< endl;
+    }else if (P[i] == 'b'){
+      cout<<"#2=2"<< endl;
+    }else if (P[i] == 'c'){
+      cout<<"#2=3"<< endl;
+    }
+
+    if(P[i] == 'a' ){
+      cout << "#2=1"<< endl;
+    }else if (P[i] == 'b'){
+      cout<<"#2=2"<< endl;
+    }else if (P[i] == 'c'){
+      cout<<"#2=3"<< endl;
+    }
+
+    if(P[i] == 'd'){
+      cout << "#3=1"<< endl;
+    }else if (P[i] == 'e'){
+      cout<<"#3=2"<< endl;
+    }else if (P[i] == 'f'){
+      cout<<"#3=3"<< endl;
+    }
+
+    if(P[i] == 'g'){
+      cout << "#4=1"<< endl;
+    }else if (P[i] == 'h'){
+      cout<<"#4=2"<< endl;
+    }else if (P[i] == 'i'){
+      cout<<"#4=3"<< endl;
+    }
+
+    if(P[i] == 'j'){
+      cout << "#5=1"<< endl;
+    }else if (P[i] == 'k'){
+      cout<<"#5=2"<< endl;
+    }else if (P[i] == 'l'){
+      cout<<"#5=3"<< endl;
+    }
+
+    if(P[i] == 'm'){
+      cout << "#6=1"<< endl;
+    }else if (P[i] == 'n'){
+      cout<<"#6=2"<< endl;
+    }else if (P[i] == 'o'){
+      cout<<"#6=3"<< endl;
+    }
+
+    if(P[i] == 'p'){
+      cout << "#7=1"<< endl;
+    }else if (P[i] == 'q'){
+      cout<<"#7=2"<< endl;
+    }else if (P[i] == 'r'){
+      cout<<"#7=3"<< endl;
+    }else if (P[i] == 's'){
+      cout << "#7=4" << endl;
+    }
+
+    if(P[i] == 't'){
+      cout << "#8=1"<< endl;
+    }else if (P[i] == 'u'){
+      cout<<"#8=2"<< endl;
+    }else if (P[i] == 'v'){
+      cout<<"#8=3"<< endl;
+    }
+
+    if(P[i] == 'w'){
+      cout << "#9=1"<< endl;
+    }else if (P[i] == 'x'){
+      cout<<"#9=2"<< endl;
+    }else if (P[i] == 'y'){
+      cout<<"#9=3"<< endl;
+    }else if( P[i] == 'z'){
+      cout << "#9=4" << endl;
+    }
+
+
+  }
+
+
 }
